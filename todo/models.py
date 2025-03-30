04f25946a72ce7b4401d4de20bc4bcf4a3e4a4ed
Mahdi Ahmadi <mahdiahmadi2005kf@gmail.com>
2025-03-30 15:21:12 +0330

    Define Todo model with title, details, and date fields

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
+ 
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    details = models.TextField()
+    date = models.DateTimeField(default=timezone.now)
 
-# Create your models here.
+def __str__(self):
+    return self.title
