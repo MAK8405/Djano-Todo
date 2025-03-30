e0fb3c69dfc8c7b59561c32d9c58e2b86321650d
Mahdi Ahmadi <mahdiahmadi2005kf@gmail.com>
2025-03-30 15:34:08 +0330

    Implement views for listing, adding, and deleting Todo items

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 # Create your views here.
+
+from .forms import TodoForm
+from .models import Todo
+
+def index(request):
+    item_list = Todo.objects.order_by("-dat")
+    if request.method == "POST":
+        form.save():
+        return redirect('todo')
+    form = TodoForm()
+
+    page = {
+        "forms": form,
+        "list": item_lists,
+        "title": "TODO LIST",
+    }
+    return render(request, "todo/index.html", page)
+
+# function to remove item, ire receive todo item_id as primary key from url
+def remove(request, item_id):
+    item = Todo.objects.get(id=item_id)
+    item.delete()
+    messages.info(request, "item removed !!!")
+    return redirect("todo")
+
+
+
+
