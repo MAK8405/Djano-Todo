d34d6919271b2600c6da479130d8759efceeea84
Mahdi Ahmadi <mahdiahmadi2005kf@gmail.com>
2025-03-30 15:45:32 +0330

    Fix issues in index view

--- todo/views.py
+++ todo/views.py
@@ -9,13 +9,13 @@ from .models import Todo
 def index(request):
     item_list = Todo.objects.order_by("-dat")
     if request.method == "POST":
-        form.save():
+        form.save()
         return redirect('todo')
     form = TodoForm()
 
     page = {
         "forms": form,
-        "list": item_lists,
+        "list": item_list,
         "title": "TODO LIST",
     }
     return render(request, "todo/index.html", page)
